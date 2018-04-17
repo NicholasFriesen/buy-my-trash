a083a4441f08d39369d82ece77d5120e7326a1f1
Nicholas Friesen <36311855+NicholasFriesen@users.noreply.github.com>
2018-04-17 09:31:55 -0600

    Update message.js
    Add 'CloseMessage()' function

--- assets/message.js
+++ assets/message.js
@@ -1,8 +1,21 @@
-function myFunction() {
-    var message = document.getElementById("tut__message")
+function Message(message, time) {
+    var msg = document.getElementById(message)
     // Add 'show' class
-    message.className = "show";
+    msg.className = "show";
 
     // After 5 seconds, remove the 'show' class
-    setTimeout(function(){ message.className = message.className.replace("show", ""); }, 5000);
+    if (time == 'false') {
+    }
+    else {
+      setTimeout(function(){ msg.className = msg.className.replace("show", ""); }, time);
+    }
 }
+function CloseMessage(message) {
+  	var msg = document.getElementById(message)
+  	
+    //remove 'show' class, add 'fade' class
+ 	msg.className = msg.classList.remove("show");
+    msg.className = "fade";
+  	console.log(msg.className);
+}
+
